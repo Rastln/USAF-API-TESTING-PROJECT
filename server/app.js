07326ae5ffc343e0786addde51e61a475c054469
Andrew Weir <yodamancan11@yahoo.com>
2020-06-30 13:36:04 -0600

    SQL working now

--- server/app.js
+++ server/app.js
@@ -1,14 +1,25 @@
+
 const fs = require("fs")
 const bodyParser = require("body-parser")
-
 const express = require('express')
 const app = express()
-const port = 3008
+const port = 4761
+app.use(bodyParser.json())
+
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'movieinfo',
+  host: 'localhost',
+  database: 'movieinfo',
+  password: '123',
+  port: 5432,
+})
 
 const movies = JSON.parse(fs.readFileSync("movies.JSON"))
 
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+
 
 app.get("/", (req, res) => {
   res.send({greeting: "Hello World"})
